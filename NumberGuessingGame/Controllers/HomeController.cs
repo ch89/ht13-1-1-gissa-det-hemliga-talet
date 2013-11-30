f38bf924e03d35886ea25d4281cc8d0e4b84d089
ch <chhq89@gmail.com>
2013-11-30 05:32:43 +0100

    first commit

---
+++ NumberGuessingGame/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SecretNumber.Models;
+
+namespace SecretNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SecretNumberModel SecretNumber
+        {
+            get
+            {
+                return (SecretNumberModel)Session["SecretNumber"];
+            }
+            set
+            {
+                Session["SecretNumber"] = value;
+            }
+        }
+
+        public ActionResult Index()
+        {
+            SecretNumber = new SecretNumberModel();
+            return View(SecretNumber);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection formData)
+        {
+            if (!Session.IsNewSession)
+            {
+                if (!TryUpdateModel(SecretNumber, formData))
+                {
+                    ModelState.AddModelError(String.Empty, "Ett fel inträffade.");
+                }
+
+                if (ModelState.IsValid)
+                {
+                    SecretNumber.MakeGuess(SecretNumber.Guess);
+                }
+            }
+            
+            return View(SecretNumber);
+        }
+    }
+}
---
+++ NumberGuessingGame/Models/SecretNumberModel.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace SecretNumber.Models
+{
+    public enum Outcome
+    {
+        Indefinite, 
+        Low, 
+        High, 
+        Correct, 
+        NoMoreGuesses, 
+        PreviousGuess
+    }
+
+    public class SecretNumberModel
+    {
+        private int number;
+        private List<int> previousGuesses;
+        public const int maxNumberOfGuesses = 7;
+
+        [DisplayName("Gissning")]
+        [Required(ErrorMessage = "Du måste ange ett tal")]
+        [Range(1, 100, ErrorMessage = "Ange ett heltal mellan 1 och 100")]
+        public int Guess { get; set; }
+        public Outcome Result { get; set; }
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+        }
+
+        public ReadOnlyCollection<int> PreviousGuesses
+        {
+            get
+            {
+                return previousGuesses.AsReadOnly();
+            }
+        }
+
+        public bool CanMakeGuess
+        {
+            get
+            {
+                return previousGuesses.Count < MaxNumberOfGuesses;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return previousGuesses.Count;
+            }
+        }
+
+        public int MaxNumberOfGuesses
+        {
+            get
+            {
+                return maxNumberOfGuesses;
+            }
+        }
+
+        public SecretNumberModel()
+        {
+            previousGuesses = new List<int>();
+            number = new Random().Next(1, 100);
+        }
+
+        public void MakeGuess(int guess)
+        {
+            Guess = guess;
+            foreach (int currentGuess in previousGuesses)
+            {
+                if (guess == currentGuess)
+                {
+                    Result = Outcome.PreviousGuess;
+                    return;
+                }
+            }
+
+            previousGuesses.Add(guess);
+
+            if (guess == number)
+            {
+                Result = Outcome.Correct;
+                return;
+            }
+
+            if (CanMakeGuess)
+            {
+                if (guess > number)
+                {
+                    Result = Outcome.High;
+                }
+                else if (guess < number)
+                {
+                    Result = Outcome.Low;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            else
+            {
+                Result = Outcome.NoMoreGuesses;
+            }
+        }
+    }
+}
